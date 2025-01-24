9343d4c6b057a88bf328e68a17026511df9ae1d4
avadhThinktanker <avadh@thinktanker.in>
2025-01-24 16:21:09 +0530

    add discount functionality

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "../navbar";
 import ProductCard from "../product-card";
@@ -6,6 +6,9 @@ import "./style.css";
 
 const CartPage = () => {
   const items = useSelector((state) => state.cart.items);
+  const ref = useRef(null);
+  const [discountAmount, setDiscountAmount] = useState(0);
+  const [error, setError] = useState("");
 
   function renderCartItems() {
     return items.map((item) => (
@@ -16,6 +19,44 @@ const CartPage = () => {
   const totalAmount = items
     .reduce((total, item) => total + item.price * item.quantity, 0)
     .toFixed(2);
+
+  const handleCoupon = () => {
+    const couponCode = ref?.current?.value;
+    console.log(couponCode);
+    if (!couponCode) {
+      setError("");
+      return;
+    }
+    switch (couponCode) {
+      case "avadh123":
+        if (totalAmount > 1000) {
+          setDiscountAmount((totalAmount * 0.9).toFixed(2));
+        } else {
+          setDiscountAmount((totalAmount - 100).toFixed(2));
+        }
+        break;
+      case "1333":
+        if (totalAmount > 1500) {
+          setDiscountAmount((totalAmount * 0.85).toFixed(2));
+        } else {
+          setDiscountAmount((totalAmount - 250).toFixed(2));
+        }
+        setError("");
+        break;
+      case "1222":
+        if (totalAmount > 2500) {
+          setDiscountAmount((totalAmount * 0.75).toFixed(2));
+        } else {
+          setDiscountAmount((totalAmount - 500).toFixed(2));
+        }
+        setError("");
+        break;
+      default:
+        setError("Invalid Coupon Code");
+        setDiscountAmount(totalAmount);
+    }
+  };
+  useEffect(() => handleCoupon(), [totalAmount]);
 
   return (
     <>
@@ -38,6 +79,22 @@ const CartPage = () => {
               <span>Total Amount: </span>
             </p>
             <span>{totalAmount}</span>
+
+            <div>
+              <input type="text" ref={ref} placeholder="Enter coupon code" />
+              <button onClick={handleCoupon}>Submit</button>
+            </div>
+
+            {error && <p className="error">{error}</p>}
+
+            {discountAmount > 0 && (
+              <div>
+                <p>
+                  <span>Total after Discount: </span>
+                  <span>{discountAmount}</span>
+                </p>
+              </div>
+            )}
           </div>
         )}
       </div>
@@ -45,4 +102,4 @@ const CartPage = () => {
   );
 };
 
-export default CartPage;
+export default React.memo(CartPage);
