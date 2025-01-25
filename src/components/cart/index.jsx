4ee646ac8de1215c052b5d6e39123d3285caedcd
avadhThinktanker <avadh@thinktanker.in>
2025-01-25 10:46:14 +0530

    implemet redux-persist

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "../navbar";
 import ProductCard from "../product-card";
@@ -6,6 +6,51 @@ import "./style.css";
 
 const CartPage = () => {
   const items = useSelector((state) => state.cart.items);
+  const ref = useRef(null);
+  const [discountAmount, setDiscountAmount] = useState(0);
+  const [error, setError] = useState("");
+
+  const totalAmount = items
+    .reduce((total, item) => total + item.price * item.quantity, 0)
+    .toFixed(2);
+
+  const handleCoupon = () => {
+    const couponCode = ref?.current?.value;
+    if (!couponCode) {
+      setError("");
+      return;
+    }
+    switch (couponCode) {
+      case "avadh123":
+        if (totalAmount > 1000) {
+          setDiscountAmount((totalAmount * 0.9).toFixed(2));
+        } else {
+          setDiscountAmount((totalAmount - 100).toFixed(2));
+        }
+        break;
+      case "1333":
+        if (totalAmount > 1500) {
+          setDiscountAmount((totalAmount * 0.85).toFixed(2));
+        } else {
+          setDiscountAmount((totalAmount - 250).toFixed(2));
+        }
+        setError("");
+        break;
+      case "1222":
+        if (totalAmount > 2500) {
+          setDiscountAmount((totalAmount * 0.75).toFixed(2));
+        } else {
+          setDiscountAmount((totalAmount - 500).toFixed(2));
+        }
+        setError("");
+        break;
+      default:
+        setError("Invalid Coupon Code");
+        setDiscountAmount(totalAmount);
+    }
+  };
+
+  useEffect(() => handleCoupon(), [totalAmount]);
 
   function renderCartItems() {
     return items.map((item) => (
@@ -13,9 +58,6 @@ const CartPage = () => {
     ));
   }
 
-  const totalAmount = items
-    .reduce((total, item) => total + item.price * item.quantity, 0)
-    .toFixed(2);
 
   return (
     <>
@@ -38,6 +80,22 @@ const CartPage = () => {
               <span>Total Amount: </span>
             </p>
             <span>{totalAmount}</span>
+
+            <div>
+              <input type="text" ref={ref} placeholder="Enter coupon code" />
+              <button onClick={handleCoupon}>Submit</button>
+            </div>
+
+            {error && <p className="error">{error}</p>}
+
+            {discountAmount > 0 && (
+              <div>
+                <p>
+                  <span>Total after Discount: </span>
+                  <span>{discountAmount}</span>
+                </p>
+              </div>
+            )}
           </div>
         )}
       </div>
@@ -45,4 +103,4 @@ const CartPage = () => {
   );
 };
 
-export default CartPage;
+export default React.memo(CartPage);
--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import CartPage from "./components/cart";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="cart" element={<CartPage />} />
-        </Routes>
+        <PersistGate loading={null} persistor={persistor}>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="cart" element={<CartPage />} />
+          </Routes>
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>
